9439f484c4ab4a40a0662bdc06db4a391dafd001
enmanuel-mctekk <enmanuel@mctekk.com>
2025-02-04 11:58:04 -0400

    feat(history): implement history list

--- src/screens/parking-history/index.tsx
+++ src/screens/parking-history/index.tsx
@@ -1,35 +1,33 @@
+/* eslint-disable react-native/no-inline-styles */
 // Modules
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 // Styles
-import {
-  ScreenHeader,
-} from './styles';
+import { ScreenHeader, Content } from './styles';
 
 // Organisms
 import ViewContainer from 'components/organisms/view-container';
+import HistoryList from 'components/organisms/history-list';
 
 // Atoms
 import { TextTransform, translate } from 'components/atoms/localized-label';
 
-interface IParkingHistoryScreenProps {
-
-}
+interface IParkingHistoryScreenProps {}
 
 const ParkingHistory = (props: IParkingHistoryScreenProps) => {
-
   // Props
   const { navigation } = props;
 
   return (
     <ViewContainer>
-
       <ScreenHeader
         title={translate('history', TextTransform.CAPITALIZE)}
         subtitle={translate('historyMsg', TextTransform.NONE)}
       />
-
+      <Content>
+        <HistoryList />
+      </Content>
     </ViewContainer>
   );
 };
