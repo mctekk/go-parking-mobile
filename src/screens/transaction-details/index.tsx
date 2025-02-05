3ecdc1b51900497627c9881cee15a9d7c7845573
enmanuel-mctekk <enmanuel@mctekk.com>
2025-02-05 12:32:25 -0400

    feat(transactions): add initial transaction detail screen

---
+++ src/screens/transaction-details/index.tsx
@@ -0,0 +1,135 @@
+/* eslint-disable react-native/no-inline-styles */
+// Modules
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+// Styles
+import {
+  ScreenHeader,
+  Content,
+  TopContainer,
+  MapContainer,
+  TopSectionContainer,
+  DetailsContainer,
+  DetailRowContainer,
+} from './styles';
+import { Typography } from 'styles';
+import { DEFAULT_THEME } from 'styles/theme';
+
+// Organisms
+import ViewContainer from 'components/organisms/view-container';
+
+// Atoms
+import { TextTransform, translate } from 'components/atoms/localized-label';
+import CustomText from 'atoms/text';
+import DashLine from 'components/atoms/dash-line';
+
+interface ITransactionDetailsScreenProps {
+  navigation: any;
+  route: any;
+}
+
+export const TransactionDetails = (props: ITransactionDetailsScreenProps) => {
+  // Props
+  const { navigation, route } = props;
+
+  // Params
+  const order = route?.params?.transactionData;
+
+  const rowsData = [
+    {
+      id: 1,
+      titleLocale: 'bookingDate',
+      subtitle: '28 July 2024',
+      bottomDashLine: false,
+    },
+    {
+      id: 2,
+      titleLocale: 'parkingNo',
+      subtitle: '#309',
+      bottomDashLine: true,
+    },
+    {
+      id: 3,
+      titleLocale: 'arriveAfter',
+      subtitle: '10:00 AM',
+      bottomDashLine: false,
+    },
+    {
+      id: 4,
+      titleLocale: 'exitBefore',
+      subtitle: '11:00 AM',
+      bottomDashLine: false,
+    },
+    {
+      id: 5,
+      titleLocale: 'duration',
+      subtitle: order?.duration_time,
+      bottomDashLine: true,
+    },
+    {
+      id: 6,
+      titleLocale: 'subtotal',
+      subtitle: `$${order?.price?.amount}`,
+      bottomDashLine: true,
+    },
+  ];
+
+  return (
+    <ViewContainer>
+      <SafeAreaView />
+      <Content>
+        <ScreenHeader
+          title={translate('transactionDetails', TextTransform.CAPITALIZE)}
+          style={{ paddingHorizontal: 0 }}
+          titleProps={{ weight: '700' }}
+        />
+        <TopContainer>
+          <MapContainer />
+          <TopSectionContainer>
+            <CustomText
+              size={Typography.FONT_SIZE_14}
+              weight="700"
+              style={{ marginBottom: 8 }}
+              color={DEFAULT_THEME.white}>
+              {order?.name}
+            </CustomText>
+            <CustomText
+              size={Typography.FONT_SIZE_10}
+              weight="500"
+              lineHeight={Typography.FONT_SIZE_14}
+              color={DEFAULT_THEME.white}>
+              {order?.street}
+            </CustomText>
+          </TopSectionContainer>
+        </TopContainer>
+        <DashLine color={DEFAULT_THEME.cardGray} />
+        <DetailsContainer>
+          {rowsData?.map((rowData: any) => {
+            return (
+              <>
+                <DetailRowContainer>
+                  <CustomText
+                    size={Typography.FONT_SIZE_10}
+                    weight="500"
+                    color={DEFAULT_THEME.dashGray}>
+                    {translate(rowData?.titleLocale, TextTransform.CAPITALIZE)}
+                  </CustomText>
+                  <CustomText
+                    size={Typography.FONT_SIZE_12}
+                    weight="600"
+                    color={DEFAULT_THEME.titleGray}>
+                    {rowData?.subtitle}
+                  </CustomText>
+                </DetailRowContainer>
+                {rowData?.bottomDashLine && (
+                  <DashLine color={DEFAULT_THEME.cardGray} />
+                )}
+              </>
+            );
+          })}
+        </DetailsContainer>
+      </Content>
+    </ViewContainer>
+  );
+};
