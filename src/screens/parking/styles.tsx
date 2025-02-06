e6ed594f7e04ee940b34d70e3d8738558b4264b8
enmanuel-mctekk <enmanuel@mctekk.com>
2025-02-06 15:41:40 -0400

    feat(sessions): update parking screen

--- src/screens/parking/styles.tsx
+++ src/screens/parking/styles.tsx
@@ -8,7 +8,6 @@ import { Colors, Typography } from 'styles';
 // Molecules
 import MainHeader from 'components/molecules/main-header';
 
-
 export const Container = styled.View`
   flex: 1;
   background-color: ${DEFAULT_THEME.background};
@@ -21,7 +20,8 @@ export const ScreenHeader = styled(MainHeader)`
 
 export const Content = styled.SafeAreaView`
   flex: 1px;
-  margin: 10px;
+  background-color: ${DEFAULT_THEME.background};
+  border-radius: 25px;
 `;
 
 export const Title = styled.Text`
@@ -47,4 +47,11 @@ export const IconContainer = styled.TouchableOpacity`
   align-items: center;
   padding-right: 16px;
   margin-top: 10px;
+`;
+
+export const TabsContainer = styled.View`
+  background-color: ${DEFAULT_THEME.transparent};
+  justify-content: center;
+  margin-top: 20px;
+  margin-horizontal: 20px;
 `;
