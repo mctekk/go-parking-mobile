6620bf22dbb1d295c446f4ecb3712886175fa7f1
enmanuel-mctekk <enmanuel@mctekk.com>
2025-02-11 12:36:23 -0400

    feat(booking): add down arrow icon

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -4,3 +4,4 @@ export { default as MasterCardIcon } from './master-card-icon';
 export { default as TimeIcon } from './time-icon';
 export { default as CarIconOutline } from './car-icon-outline';
 export { default as LocationIcon } from './location-icon';
+export { default as DownArrow } from './down-arrow';
