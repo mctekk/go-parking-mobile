30d78b5ad5016887f4d655e49202b4b5851a5912
enmanuel-mctekk <enmanuel@mctekk.com>
2025-02-19 14:30:45 -0400

    Update saved-list.tsx

--- src/components/organisms/saved-list.tsx
+++ src/components/organisms/saved-list.tsx
@@ -240,7 +240,6 @@ const SavedList = (props: ISavedListProps) => {
 
   const renderItem = useCallback(
     ({ item, index }) => {
-      //return <HistoryCard key={item.id} order={item} onPress={() => onCardPress(item)} />;
       return (
         <MapsParkingCards
           id={item.id}
