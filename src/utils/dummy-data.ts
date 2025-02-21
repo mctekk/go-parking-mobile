da9ac87087a22b17125929391ed59933077ee093
enmanuel-mctekk <enmanuel@mctekk.com>
2025-02-21 11:39:20 -0400

    feat(vehicles): add dummy vehicles data

--- src/utils/dummy-data.ts
+++ src/utils/dummy-data.ts
@@ -158,351 +158,371 @@ export const vehicleTypes = [
   },
 ];
 
-
 export const Original_DummyData = [
   {
-    'id': 1,
-    'name': 'Parking Multipiso Covadonga',
-    'address': '104 Paseo Covadonga, PR-38 Calle Juan Antonio Corretjer, San Juan, PR 00901',
-    'phone': '(787) 721-0115',
-    'coordinates': {
-      'latitude': 18.465235,
-      'longitude': -66.111265,
+    id: 1,
+    name: 'Parking Multipiso Covadonga',
+    address: '104 Paseo Covadonga, PR-38 Calle Juan Antonio Corretjer, San Juan, PR 00901',
+    phone: '(787) 721-0115',
+    coordinates: {
+      latitude: 18.465235,
+      longitude: -66.111265,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 4.50,
-      'currency': 'USD',
+    price: {
+      amount: 4.5,
+      currency: 'USD',
     },
-    'totalParkingSpaces': 100,
-    'occupiedParkingSpaces': 50,
-    'isPrivate': true,
+    totalParkingSpaces: 100,
+    occupiedParkingSpaces: 50,
+    isPrivate: true,
   },
   {
-    'id': 2,
-    'name': 'Estacionamiento La Puntilla',
-    'address': 'Paseo de la Princesa, FV7M+57M, Calle C. Arturo Schomburg, San Juan, PR 00901',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.463449,
-      'longitude': -66.117866,
+    id: 2,
+    name: 'Estacionamiento La Puntilla',
+    address: 'Paseo de la Princesa, FV7M+57M, Calle C. Arturo Schomburg, San Juan, PR 00901',
+    phone: null,
+    coordinates: {
+      latitude: 18.463449,
+      longitude: -66.117866,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 3.75,
-      'currency': 'USD',
+    price: {
+      amount: 3.75,
+      currency: 'USD',
     },
-    'totalParkingSpaces': 100,
-    'occupiedParkingSpaces': 50,
-    'isPrivate': true,
+    totalParkingSpaces: 100,
+    occupiedParkingSpaces: 50,
+    isPrivate: true,
   },
   {
-    'id': 3,
-    'name': 'Parking Doña Fela',
-    'address': 'Calle del Recinto Sur 318, San Juan, Puerto Rico. 00901',
-    'phone': '(787) 722-3558',
-    'coordinates': {
-      'latitude': 18.464303,
-      'longitude': -66.114154,
+    id: 3,
+    name: 'Parking Doña Fela',
+    address: 'Calle del Recinto Sur 318, San Juan, Puerto Rico. 00901',
+    phone: '(787) 722-3558',
+    coordinates: {
+      latitude: 18.464303,
+      longitude: -66.114154,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 2.50,
-      'currency': 'USD',
+    price: {
+      amount: 2.5,
+      currency: 'USD',
     },
-    'totalParkingSpaces': 100,
-    'occupiedParkingSpaces': 50,
-    'isPrivate': true,
+    totalParkingSpaces: 100,
+    occupiedParkingSpaces: 50,
+    isPrivate: true,
   },
   {
-    'id': 4,
-    'name': 'Parking Plaza de la Convalecencia',
-    'address': '9XW2+Q5C, C. Georgetti, San Juan, 00925',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.397651,
-      'longitude': -66.052274,
+    id: 4,
+    name: 'Parking Plaza de la Convalecencia',
+    address: '9XW2+Q5C, C. Georgetti, San Juan, 00925',
+    phone: null,
+    coordinates: {
+      latitude: 18.397651,
+      longitude: -66.052274,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 4.00,
-      'currency': 'USD',
+    price: {
+      amount: 4.0,
+      currency: 'USD',
     },
-    'totalParkingSpaces': 100,
-    'occupiedParkingSpaces': 50,
-    'isPrivate': true,
+    totalParkingSpaces: 100,
+    occupiedParkingSpaces: 50,
+    isPrivate: true,
   },
   {
-    'id': 5,
-    'name': 'Estacionamiento Av. Magdalena',
-    'address': '1361 Av. Magdalena, San Juan, PR 00907',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.454311,
-      'longitude': -66.068553,
+    id: 5,
+    name: 'Estacionamiento Av. Magdalena',
+    address: '1361 Av. Magdalena, San Juan, PR 00907',
+    phone: null,
+    coordinates: {
+      latitude: 18.454311,
+      longitude: -66.068553,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 3.25,
-      'currency': 'USD',
+    price: {
+      amount: 3.25,
+      currency: 'USD',
     },
-    'totalParkingSpaces': 100,
-    'occupiedParkingSpaces': 50,
-    'isPrivate': true,
+    totalParkingSpaces: 100,
+    occupiedParkingSpaces: 50,
+    isPrivate: true,
   },
   {
-    'id': 6,
-    'name': 'Public Parking Ashford',
-    'address': '1390 Av. Ashford, San Juan, PR 00907',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.4548151,
-      'longitude': -66.0696033,
+    id: 6,
+    name: 'Public Parking Ashford',
+    address: '1390 Av. Ashford, San Juan, PR 00907',
+    phone: null,
+    coordinates: {
+      latitude: 18.4548151,
+      longitude: -66.0696033,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 4.75,
-      'currency': 'USD',
+    price: {
+      amount: 4.75,
+      currency: 'USD',
     },
-    'totalParkingSpaces': 100,
-    'occupiedParkingSpaces': 50,
-    'isPrivate': true,
+    totalParkingSpaces: 100,
+    occupiedParkingSpaces: 50,
+    isPrivate: true,
   },
   {
-    'id': 7,
-    'name': 'Estacionamiento Parroquia Stella Maris',
-    'address': '1359 Calle Luchetti San Juan, PR 00907',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.4536746,
-      'longitude': -66.0713217,
+    id: 7,
+    name: 'Estacionamiento Parroquia Stella Maris',
+    address: '1359 Calle Luchetti San Juan, PR 00907',
+    phone: null,
+    coordinates: {
+      latitude: 18.4536746,
+      longitude: -66.0713217,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 3.00,
-      'currency': 'USD',
+    price: {
+      amount: 3.0,
+      currency: 'USD',
     },
-    'totalParkingSpaces': 100,
-    'occupiedParkingSpaces': 50,
-    'isPrivate': true,
+    totalParkingSpaces: 100,
+    occupiedParkingSpaces: 50,
+    isPrivate: true,
   },
   {
-    'id': 8,
-    'name': 'Estacionamiento Ashford Laguna Lot',
-    'address': '1309 Ave. Ashford, San Juan, PR 00907',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.4560931,
-      'longitude': -66.0730023,
+    id: 8,
+    name: 'Estacionamiento Ashford Laguna Lot',
+    address: '1309 Ave. Ashford, San Juan, PR 00907',
+    phone: null,
+    coordinates: {
+      latitude: 18.4560931,
+      longitude: -66.0730023,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 2.75,
-      'currency': 'USD',
+    price: {
+      amount: 2.75,
+      currency: 'USD',
     },
-    'totalParkingSpaces': 100,
-    'occupiedParkingSpaces': 50,
-    'isPrivate': true,
+    totalParkingSpaces: 100,
+    occupiedParkingSpaces: 50,
+    isPrivate: true,
   },
   {
-    'id': 9,
-    'name': 'Estacionamiento Terra Luna Parking',
-    'address': '1050 Ave. Ashford, San Juan, PR 00907',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.4570989,
-      'longitude': -66.0730023,
+    id: 9,
+    name: 'Estacionamiento Terra Luna Parking',
+    address: '1050 Ave. Ashford, San Juan, PR 00907',
+    phone: null,
+    coordinates: {
+      latitude: 18.4570989,
+      longitude: -66.0730023,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 4.25,
-      'currency': 'USD',
+    price: {
+      amount: 4.25,
+      currency: 'USD',
     },
-    'totalParkingSpaces': 100,
-    'occupiedParkingSpaces': 50,
-    'isPrivate': true,
+    totalParkingSpaces: 100,
+    occupiedParkingSpaces: 50,
+    isPrivate: true,
   },
   {
-    'id': 10,
-    'name': 'Estacionamiento 1019 Ahford Lot',
-    'address': '1019 Avenida Ashford, San Juan, PR 00907',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.4605432,
-      'longitude': -66.0795357,
+    id: 10,
+    name: 'Estacionamiento 1019 Ahford Lot',
+    address: '1019 Avenida Ashford, San Juan, PR 00907',
+    phone: null,
+    coordinates: {
+      latitude: 18.4605432,
+      longitude: -66.0795357,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 3.50,
-      'currency': 'USD',
+    price: {
+      amount: 3.5,
+      currency: 'USD',
     },
-    'totalParkingSpaces': 100,
-    'occupiedParkingSpaces': 50,
-    'isPrivate': true,
+    totalParkingSpaces: 100,
+    occupiedParkingSpaces: 50,
+    isPrivate: true,
   },
   {
-    'id': 11,
-    'name': 'Estacionamiento José de Diego Parking',
-    'address': '405 Av. José de Diego, San Juan, PR 00912',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.4124865,
-      'longitude': -66.1070241,
+    id: 11,
+    name: 'Estacionamiento José de Diego Parking',
+    address: '405 Av. José de Diego, San Juan, PR 00912',
+    phone: null,
+    coordinates: {
+      latitude: 18.4124865,
+      longitude: -66.1070241,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 2.25,
-      'currency': 'USD',
+    price: {
+      amount: 2.25,
+      currency: 'USD',
     },
-    'totalParkingSpaces': 100,
-    'occupiedParkingSpaces': 50,
-    'isPrivate': true,
+    totalParkingSpaces: 100,
+    occupiedParkingSpaces: 50,
+    isPrivate: true,
   },
   {
-    'id': 12,
-    'name': 'Calle 01 - 125 plazas',
-    'address': 'Av. Ashford, Esquina 1305 Av. Magdalena. San Juan, PR 00907',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.454615,
-      'longitude': -66.0725578,
+    id: 12,
+    name: 'Calle 01 - 125 plazas',
+    address: 'Av. Ashford, Esquina 1305 Av. Magdalena. San Juan, PR 00907',
+    phone: null,
+    coordinates: {
+      latitude: 18.454615,
+      longitude: -66.0725578,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 3.75,
-      'currency': 'USD',
+    price: {
+      amount: 3.75,
+      currency: 'USD',
     },
-    'isPrivate': false,
+    isPrivate: false,
   },
   {
-    'id': 13,
-    'name': 'Calle 02 - 200 plazas',
-    'address': '1369 Av. Ashford, San Juan, 00907, Esq. 13 Calle Candina. San Juan, PR 00907',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.4552539,
-      'longitude': -66.0702639,
+    id: 13,
+    name: 'Calle 02 - 200 plazas',
+    address: '1369 Av. Ashford, San Juan, 00907, Esq. 13 Calle Candina. San Juan, PR 00907',
+    phone: null,
+    coordinates: {
+      latitude: 18.4552539,
+      longitude: -66.0702639,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 4.50,
-      'currency': 'USD',
+    price: {
+      amount: 4.5,
+      currency: 'USD',
     },
-    'isPrivate': false,
+    isPrivate: false,
   },
   {
-    'id': 14,
-    'name': 'Calle 03 - 100 plazas',
-    'address': '1337 Av. Ashford, Esq. 53 Calle Cervantes. San Juan, PR 00907',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.4552081,
-      'longitude': -66.0739698,
+    id: 14,
+    name: 'Calle 03 - 100 plazas',
+    address: '1337 Av. Ashford, Esq. 53 Calle Cervantes. San Juan, PR 00907',
+    phone: null,
+    coordinates: {
+      latitude: 18.4552081,
+      longitude: -66.0739698,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 2.00,
-      'currency': 'USD',
+    price: {
+      amount: 2.0,
+      currency: 'USD',
     },
-    'isPrivate': false,
+    isPrivate: false,
   },
   {
-    'id': 15,
-    'name': 'Calle 04 - 250 plazas',
-    'address': '1310 Av. Ashford & 55 Calle Caribe. San Juan, PR 00907',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.455481,
-      'longitude': -66.070431,
+    id: 15,
+    name: 'Calle 04 - 250 plazas',
+    address: '1310 Av. Ashford & 55 Calle Caribe. San Juan, PR 00907',
+    phone: null,
+    coordinates: {
+      latitude: 18.455481,
+      longitude: -66.070431,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 3.25,
-      'currency': 'USD',
+    price: {
+      amount: 3.25,
+      currency: 'USD',
     },
-    'isPrivate': false,
+    isPrivate: false,
   },
   {
-    'id': 16,
-    'name': 'Calle 05 - 250 plazas',
-    'address': 'Intersección Av. Ashford & 13 Calle Delcasse. San Juan, PR 00907',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.4571632,
-      'longitude': -66.0748954,
+    id: 16,
+    name: 'Calle 05 - 250 plazas',
+    address: 'Intersección Av. Ashford & 13 Calle Delcasse. San Juan, PR 00907',
+    phone: null,
+    coordinates: {
+      latitude: 18.4571632,
+      longitude: -66.0748954,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 4.00,
-      'currency': 'USD',
+    price: {
+      amount: 4.0,
+      currency: 'USD',
     },
-    'isPrivate': false,
+    isPrivate: false,
   },
   {
-    'id': 17,
-    'name': 'Calle 06 - 233 plazas',
-    'address': '1504 Ave. Ashford & 57 C. F. Krug. San Juan, PR 00907',
-    'phone': null,
-    'coordinates': {
-      'latitude': 18.454073,
-      'longitude': -66.062022,
+    id: 17,
+    name: 'Calle 06 - 233 plazas',
+    address: '1504 Ave. Ashford & 57 C. F. Krug. San Juan, PR 00907',
+    phone: null,
+    coordinates: {
+      latitude: 18.454073,
+      longitude: -66.062022,
     },
-    'parkingHours': {
-      'open': '06:00',
-      'close': '22:00',
+    parkingHours: {
+      open: '06:00',
+      close: '22:00',
     },
-    'price': {
-      'amount': 2.50,
-      'currency': 'USD',
+    price: {
+      amount: 2.5,
+      currency: 'USD',
     },
-    'isPrivate': false,
+    isPrivate: false,
+  },
+];
+
+export const dummyVehicleList = [
+  {
+    id: 1,
+    brand: 'Tesla',
+    model: 'Model S',
+    plate: 'EF479379',
+  },
+  {
+    id: 2,
+    brand: 'Mercedes Benz',
+    model: 'GLE 2022',
+    plate: 'AF897541',
+  },
+  {
+    id: 3,
+    brand: 'Honda',
+    model: 'Civic',
+    plate: 'EF479379',
   },
 ];
